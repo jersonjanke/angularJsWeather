5d2766866194d8b06d15319ee29ef50c5cb9df5b
Jerson Janke <jersonjanke@gmail.com>
2017-08-30 20:46:57 -0300

    Update footer

--- app/app.module.js
+++ app/app.module.js
@@ -24,6 +24,7 @@ app.config(['$translateProvider', function ($translateProvider) {
   $translateProvider.translations('en', {
     'Home': 'Home',
     'About': 'About',
+    'Sobre': 'About',
     'Contact': 'Contact',
     'Search': 'Search',
     'City': 'City',
@@ -48,6 +49,7 @@ app.config(['$translateProvider', function ($translateProvider) {
     'Linkedin:': 'Linkedin:',
     'Objetivo': 'Objective',
     'Tecnologia': 'Technology',
+    'Desenvolvido por:' : 'Developed by:',
     'Para consultar os dados previsão do tempo foi utilizada um API que retorna um json(javascript object notation) com os dados da previsão. API utilizada foi': 'To query the weather forecast data we used an API that returns a json (javascript object notation) with the forecast data. API used was',
     'Objetivo deste protótipo é realizar consulta da previsão do tempo com base na cidade e estado desejada pelo usuário. A página exibirá além da temperatura máxima e mínima do dia atual a previsão dos próximos 7 dias da semana. Onde a mesa será dividia em 8 box. Inciando pelo Box de consulta de Estado e Cidade, onde inicialmente o usuário selecionará o estado para selecionar posteriormente a cidade desejada. E por último o Box do gráfico de variação de temperatura da semana, exibirá um gráfico linear com máximas e mínimas dos dias exibidos.' : 'Purpose of this prototype is to perform weather forecast consultation based on the city and state desired by the user. The page will display beyond the maximum and minimum temperature of the current day the forecast of the next 7 days of the week. Where the table would be divided into 8 boxes. Inciando by the State and City query box, where initially the user will select the state to select the desired city later. And finally the Box of the temperature variation chart of the week, will display a linear graph with maximum and minimum of the days displayed'
   });
@@ -79,6 +81,7 @@ app.config(['$translateProvider', function ($translateProvider) {
     'Linkedin:': 'Linkedin:',
     'Objetivo': 'Objetivo',
     'Tecnologia': 'Tecnologia',
+    'Desenvolvido por:' : 'Desenvolvido por:',
     'Para consultar os dados previsão do tempo foi utilizada um API que retorna um json(javascript object notation) com os dados da previsão. API utilizada foi': 'Para consultar os dados previsão do tempo foi utilizada um API que retorna um json(javascript object notation) com os dados da previsão. API utilizada foi',
     'Objetivo deste protótipo é realizar consulta da previsão do tempo com base na cidade e estado desejada pelo usuário. A página exibirá além da temperatura máxima e mínima do dia atual a previsão dos próximos 7 dias da semana. Onde a mesa será dividia em 8 box. Inciando pelo Box de consulta de Estado e Cidade, onde inicialmente o usuário selecionará o estado para selecionar posteriormente a cidade desejada. E por último o Box do gráfico de variação de temperatura da semana, exibirá um gráfico linear com máximas e mínimas dos dias exibidos' : 'Objetivo deste protótipo é realizar consulta da previsão do tempo com base na cidade e estado desejada pelo usuário. A página exibirá além da temperatura máxima e mínima do dia atual a previsão dos próximos 7 dias da semana. Onde a mesa será dividia em 8 box. Inciando pelo Box de consulta de Estado e Cidade, onde inicialmente o usuário selecionará o estado para selecionar posteriormente a cidade desejada. E por último o Box do gráfico de variação de temperatura da semana, exibirá um gráfico linear com máximas e mínimas dos dias exibidos'
   });
